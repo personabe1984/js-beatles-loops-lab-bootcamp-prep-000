14bffe01463477269b8907ee7131baaf47f41199
Hanbi Hans Choi <personabe1984@gmail.com>
2018-05-22 03:41:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 function johnLennonFacts(facts) {
-  let n = facts.length-1;
+  let n = facts.length;
   
   while(n > 0) {
     facts[n] = facts[n] + "!!!";
