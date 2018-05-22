292f5f46a1eff0d95acfaa0a7896f6d6d2998f0a
Hanbi Hans Choi <personabe1984@gmail.com>
2018-05-22 03:37:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,4 +19,11 @@ function johnLennonFacts(facts) {
 function iLoveTheBeatles() {
   
 }
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
 
+console.log(johnLennonFacts(facts));
